055b5ce2a25ad071a5e1da0de4d18eea8cad712d
nelsonchege <68642235+nelsonchege@users.noreply.github.com>
2021-08-06 12:41:04 +0300

    Update Details.js
    to be able to save input from users and save them in flask backend

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,30 @@
 import './Details.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function Details (){
+  const [username,setUsername] = useState(null)
+  const [email,setEmail] = useState(null)
+  const [phone_number,setPhone_number] = useState(null)
+  const [first_name,setFirst_name] = useState(null)
+  const [second_name,setSecond_name] = useState(null)
+  const [radio,setRadio] = useState(null)
+  const [birthday,setBirthday] = useState(null)
+  function handleDetails(e){
+    e.preventDefault()
+    axios.post('http://127.0.0.1:5000/createprofile',{
+      'username' : username,
+      'phone_number' : phone_number,
+      'email' : email,
+      'first_name': first_name,
+      'second_name' : second_name,
+      'birthday' : birthday,
+      'radio': radio
+    })
+    .then(console.log('new profile created'))
+    .catch(err=> console.error('no profile created',err))
+
+  }
     return ( 
 <div className="main_div">
 <div className="signup-container">
@@ -12,65 +36,59 @@ function Details (){
 
   </div>
 </div>
-<div className="right-container">
+<form onSubmit = {handleDetails} >
+  <div className="right-container">
 <header>
 <h1>profile </h1>
 <div className="set">
 <div className="username">
   <label for="username">username</label>
-  <input  placeholder="username" type="text"></input>
-</div>
-<div className="user-photo">
-  <button id="user-upload">
-
-    <i className="fa fa-car"></i>
-  </button>
-  <label for="user-upload">Upload a photo</label>
+  <input  placeholder="username" type="username" value={username || ''} onChange = {(e)=> setUsername(e.target.value)}></input>
 </div>
 </div>
 <div className="set">
   <div className="firstName">
     <label for="firstName"> first Name</label>
-    <input placeholder="first Name" type="text"></input>
+    <input placeholder="first Name" type="first_name" value={first_name || ''} onChange = {(e)=> setFirst_name(e.target.value)}></input>
   </div>
   <div className="secondName">
     <label for="secondName"> second Name</label>
-    <input placeholder="second Name" type="text"></input>
+    <input placeholder="second Name" type="second_name" value={second_name || ''} onChange = {(e)=> setSecond_name(e.target.value)}></input>
   </div>
 </div>
 <div className="set">
 <div className="Gender">
   <label for="Gender-female">Gender</label>
   <div className="radio-container">
-    <input id="Gender-female" name="Gender" type="radio" value="female"></input>
+    <input id="Gender-female" name="Gender" type="radio" value={radio || 'female'} onChange = {(e)=> setRadio(e.target.value)}></input>
     <label for="Gender-female">Female</label>
-    <input id="Gender-male" name="Gender" type="radio" value="male"></input>
+    <input id="Gender-male" name="Gender" type="radio" value={radio || 'male'} onChange = {(e)=> setRadio(e.target.value)}></input>
     <label for="Gender-male">Male</label>
   </div>
 </div>
 <div className="birthday">
   <label for="birthday">Birthday</label>
-  <input placeholder="MM/DD/YYYY" type="text"></input>
+  <input placeholder="MM/DD/YYYY" type="birthday" value={birthday || ''} onChange = {(e)=> setBirthday(e.target.value)}></input>
 </div>
 </div>
 <div className="set">
   <div class="phoneNumber">
     <label for="phoneNumber">phone Number</label>
-    <input placeholder="phone Number" type="text"></input>
+    <input placeholder="phone Number" type="phone_number" value={phone_number || ''} onChange = {(e)=> setPhone_number(e.target.value)}></input>
   </div>
   <div className="email">
     <label for="email">email</label>
-    <input placeholder="email" type="text"></input>
+    <input placeholder="email" type="email" value={email || ''} onChange = {(e)=> setEmail(e.target.value)}></input>
   </div>
 </div>
 </header>
 <footer>
 <div className="set">
-<button id="back">update</button>
-<button id="next">save</button>
+<button type="submit" >save</button>
 </div>
 </footer>
 </div>
+</form>
 </div>
  </div>
      );
